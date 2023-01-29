7f16b7c7105e24d643d5ecf924bec9556d41f3ee
imcskkk <1251737072@qq.com>
2023-01-29 23:40:51 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import CustomForm from "./components/CustomForm.jsx";
+import EditForm from "./components/EditForm.jsx";
+import TaskList from "./components/TaskList.jsx";
+import useLocalStorage from './hooks/useLocalStorage';
+function App() {
+    const [tasks,setTasks] = useLocalStorage('react-todo.tasks',[]);
+    const [PreviousFocusEl,setPreviousFocusEl] = useState(null);
+    const [editedTask,setEditedTask] = useState(null);
+    const [isEditing,setIsEditing] = useState(false);
+
+  const addTask=(task)=>{
+      setTasks(prevState => [...prevState,task])
+  }
+const deleteTask=(id)=>{
+      setTasks(prevState => prevState.filter(t=>t.id!==id));
+}
+const toggleTask=(id)=>{
+      setTasks(prevState => prevState.map(t=>(
+          t.id===id
+            ?{ ...t,checked:!t.checked}
+            :t
+      )))
+}
+    const updateTask=(task)=>{
+        setTasks(prevState => prevState.map(t=>(
+            t.id===task.id
+                ?{ ...t,name:task.name}
+                :t
+        )))
+        closeEditMode();
+    }
+
+    const closeEditMode=()=>{
+      setIsEditing(false);
+      PreviousFocusEl.focus();
+    }
+    const enterEditMode=(task)=>{
+      setEditedTask(task);
+      setIsEditing(true);
+      setPreviousFocusEl(document.activeElement);
+    }
+  return (
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+        {
+            isEditing && (
+            <EditForm
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+            />
+            )
+        }
+      <CustomForm addTask={addTask}/>
+        {tasks && (
+            <TaskList
+                tasks={tasks}
+                deleteTask={deleteTask}
+                toggleTask={toggleTask}
+                enterEditMode={enterEditMode}
+            />
+            )}
+    </div>
+  )
+}
+
+export default App
